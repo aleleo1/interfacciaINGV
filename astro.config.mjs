d10686b7a22415bc788248579b59d57bfc66de9a
aleleo1 <100156426+aleleo1@users.noreply.github.com>
2023-12-18 09:49:46 +0100

    added base and site

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aleleo1.github.io',
+  base: '/interfacciaINGV',
   output: "server",
   integrations: [solidJs(), tailwind()],
   adapter: node({
