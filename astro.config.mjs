a4644a06bcb5af4d494172ee2a2ed1762078fcf1
aleleo1 <100156426+aleleo1@users.noreply.github.com>
2023-12-18 19:44:31 +0100

    test host on vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
-
 import node from "@astrojs/node";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://aleleo1.github.io',
-  base: '/interfacciaINGV',
   output: "server",
   integrations: [solidJs(), tailwind()],
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel()
 });
